22961c218d8f05dc949319082a391ec66f10372c
tstephen22 <78868509+tstephen22@users.noreply.github.com>
2021-10-30 00:00:10 +0100

    Add files via upload
    Added some DAG handling of a Binary DAG.

--- BinaryTree.java
+++ BinaryTree.java
@@ -13,6 +13,10 @@ class Node
  
 public class BinaryTree
 {
+	private Node LCA; 
+	private int n1Len = 0; 
+	private int n2Len = 0; 
+	private int lcaLen = 0; 
     // Root of the Binary Tree
     Node root;
     static boolean present1 = false, present2 = false;
@@ -21,7 +25,7 @@ public class BinaryTree
      * findLCA(Node node, int n1, int n2) - finds the LCA of two integer nodes n1 and n2
      * - Returns LCA if both are present and null otherwise 
      */
-    private Node findLCA(Node node, int n1, int n2)
+    private Node findLCA(Node node, int n1, int n2, int lcaLength)
     {
         if (node == null)
             return null;
@@ -34,24 +38,35 @@ public class BinaryTree
         {
             present1 = true;
             temp = node;
+            n1Len = lcaLength; 
         }
         if (node.data == n2)
         {
             present2 = true;
             temp = node;
+            n2Len = lcaLength; 
         }
  
         // Look for keys in left and right subtrees
-        Node leftLCA = findLCA(node.left, n1, n2);
-        Node rightLCA = findLCA(node.right, n1, n2);
+        Node leftLCA = findLCA(node.left, n1, n2, lcaLength+1);
+        Node rightLCA = findLCA(node.right, n1, n2, lcaLength+1);
  
         if (temp != null)
             return temp;
  
         //If leftLCA & rightLCA return Non-Null, node is the LCA
-        if (leftLCA != null && rightLCA != null)
-            return node;
- 
+        if (leftLCA != null && rightLCA != null) {
+           if(LCA == null) {//If LCA is not found yet, set LCA to newly found LCA 
+        	   LCA = node; 
+        	   lcaLen = n1Len + n2Len; 
+           } else { //If another LCA is found, compare lengths of the two LCAs and assign to the lower of the 2
+        	   if(lcaLen > (n1Len+n2Len)) {
+        		   LCA = node; 
+        		   lcaLen = n1Len+n2Len; 
+        	   }
+           }
+           return node; 
+        }
         // Otherwise check if left subtree or right subtree is LCA
         return (leftLCA != null) ? leftLCA : rightLCA;
     }
@@ -64,10 +79,12 @@ public class BinaryTree
         present2 = false;
  
         // Find lca 
-        Node lca = findLCA(root, n1, n2);
+        Node lca = findLCA(root, n1, n2, 0);
  
         // Return LCA only if both n1 and n2 are present in tree
-        if (present1 && present2) return lca; else return null;
+        Node returnedLCA = LCA;
+        LCA = null; 
+        if (present1 && present2) return returnedLCA; else return null;
         // Else return NULL
     }
 }
--- TestUnitLCA.java
+++ TestUnitLCA.java
@@ -8,7 +8,6 @@ public class TestUnitLCA {
 	public void test() {
 		BinaryTree bt = new BinaryTree(); 
 		bt.root = new Node(1); 
-		bt.root = new Node(1);
 		bt.root.left = new Node(2);
 		bt.root.right = new Node(3);
 		bt.root.left.left = new Node(4);
@@ -22,7 +21,30 @@ public class TestUnitLCA {
 		//Test for non-existent LCA between 10, 2 in param 1
 		assertEquals(bt.findLCA(10, 2), null); 
 		//Test for root = LCA
-		assertEquals(bt.findLCA(7, 4), bt.root); 
+		assertEquals(bt.findLCA(7, 4), bt.root);
+		//Test on DSG with 2 nodes sharing same 2 parents (same length)
+		bt = new BinaryTree(); 
+		bt.root = new Node(1);
+		bt.root.left = new Node(2);
+		bt.root.right = new Node(3); 
+		bt.root.left.right = new Node(4); 
+		bt.root.right.left = bt.root.left.right; 
+		bt.root.right.right = new Node(5);
+		bt.root.left.left = bt.root.right.right;
+		assertEquals(bt.findLCA(4, 5).data, 2);
+		//Test on DSG where lengths are different 
+		bt = new BinaryTree(); 
+		bt.root = new Node(1); 
+		bt.root.left = new Node(2);
+		bt.root.left.left = new Node(4); 
+		bt.root.right = new Node(3); 
+		bt.root.right.left = new Node(5);
+		bt.root.right.left.left = new Node(7);
+		bt.root.right.left.right = new Node(8); 
+		bt.root.right.right = new Node(6);
+		bt.root.left.left.left = bt.root.right.right; 
+		assertEquals(bt.findLCA(6, 7).data, 3); 
+		
 	}
 
 }
